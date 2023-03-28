99cbb2fc362565bd84109d9fbe7706f43ee317db
gongqianjun <gongqianjun@byteplan.com>
2023-03-28 15:14:31 +0800

    更改ci和标题

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://cloud.byteplan.com/static/logo@2x.f6190737.png',
+      name: 'BytePlan',
+      description: '一个由OpenAI开发的大型语言模型',
     },
   }
 }
